e71dc5e98a2d3b0ecf08a8799d68e8e3ebc3a896
DucDH <ducdhse130442@fpt.edu.vn>
2020-04-11 21:34:54 +0700

    Added backend firebase id token authentication

---
+++ src/main/java/com/ducdh/ticket/service/impl/FirebaseUserServiceImpl.java
@@ -0,0 +1,37 @@
+package com.ducdh.ticket.service.impl;
+
+import com.ducdh.ticket.model.exception.FirebaseUnauthorizedException;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthException;
+import com.google.firebase.auth.FirebaseToken;
+import com.google.firebase.auth.UserRecord;
+import com.google.firebase.auth.UserRecord.CreateRequest;
+import org.springframework.stereotype.Service;
+
+@Service
+public class FirebaseUserServiceImpl {
+
+    public FirebaseToken verifyIdToken(String idToken) {
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        try {
+            return auth.verifyIdToken(idToken);
+        } catch (FirebaseAuthException e) {
+            throw new FirebaseUnauthorizedException("Unauthorized");
+        }
+    }
+
+    public String createFirebaseUser(String email) {
+        FirebaseAuth auth = FirebaseAuth.getInstance();
+        CreateRequest request = new CreateRequest()
+                .setEmail(email)
+                .setEmailVerified(true);
+        UserRecord userRecord;
+        try {
+            userRecord = auth.createUser(request);
+        } catch (FirebaseAuthException e) {
+            System.out.println(e.getErrorCode() + " - " + e.getMessage());
+            return "";
+        }
+        return userRecord.getUid();
+    }
+}
