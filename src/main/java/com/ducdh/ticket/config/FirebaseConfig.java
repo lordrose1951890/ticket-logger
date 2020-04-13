cd4a3b0f86590c83400d21ad85f6a45fd27049de
DucDH <ducdhse130442@fpt.edu.vn>
2020-04-13 11:07:22 +0700

    Adjusted FirebaseConfig

--- src/main/java/com/ducdh/ticket/config/FirebaseConfig.java
+++ src/main/java/com/ducdh/ticket/config/FirebaseConfig.java
@@ -3,6 +3,7 @@ package com.ducdh.ticket.config;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
+import org.jcp.xml.dsig.internal.SignerOutputStream;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
@@ -15,14 +16,18 @@ public class FirebaseConfig {
 
     @Primary
     @Bean
-    public void init() throws IOException {
-        FileInputStream stream = new FileInputStream("firebase-adminsdk.json");
+    public void init() {
+        try {
+            FileInputStream stream = new FileInputStream("firebase-adminsdk.json");
 
-        FirebaseOptions options = new FirebaseOptions.Builder()
-                .setCredentials(GoogleCredentials.fromStream(stream))
-                .setDatabaseUrl("https://demofirebase-c1df7.firebaseio.com")
-                .build();
+            FirebaseOptions options = new FirebaseOptions.Builder()
+                    .setCredentials(GoogleCredentials.fromStream(stream))
+                    .setDatabaseUrl("https://demofirebase-c1df7.firebaseio.com")
+                    .build();
 
-        FirebaseApp.initializeApp(options);
+            FirebaseApp.initializeApp(options);
+        } catch (IOException e) {
+            System.out.println(">>>>>>>>>> Firebase: " + e.getLocalizedMessage());
+        }
     }
 }
